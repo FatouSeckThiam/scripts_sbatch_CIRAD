ab73d8eaee474f90ef82cf84bb624759f3f051ee
Fatou Seck Thiam <86953064+FatouSeckThiam@users.noreply.github.com>
2021-11-05 16:41:53 +0100

    Update mapping_nuclear.sh

--- mapping_nuclear.sh
+++ mapping_nuclear.sh
@@ -32,7 +32,8 @@ module load samtools/1.2
 
 ###bowtie2 -f -x Dalata -U Baits_Capture_RADlocus_Alata-Numularia-1.fasta -S mapping_Dalata_baits.sam
 ### l'option -f correspond au infput format fasta et l'option -q correspond aux input format fatq (comme les reads par rexemple)
-for infile in /home/thiamf/work_agap/DEFI/R1_R2_nuclear_data_trim/*1P.trimmed.fastq.gz; do base=$(echo ${infile} | sed "s/_1P.trimmed\.fastq\.gz//"); echo ${base}_1P.trimmed.fastq.gz ${base}_2P.trimmed.fastq.gz
+for infile in /home/thiamf/work_agap/DEFI/R1_R2_nuclear_data_trim/*1P.trimmed.fastq.gz; do
+base=$(echo ${infile} | sed "s/_1P.trimmed\.fastq\.gz//"); echo ${base}_1P.trimmed.fastq.gz ${base}_2P.trimmed.fastq.gz
 bowtie2 -p 4 -x Dalata_chloro -1 ${base}_1P.trimmed.fastq.gz -2 ${base}_2P.trimmed.fastq.gz | samtools view -bS - > ${base}.bam
 done
 
