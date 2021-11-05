ae8f45cf77dbc332197ef79c8114420ebb101613
Fatou Seck Thiam <86953064+FatouSeckThiam@users.noreply.github.com>
2021-11-05 17:24:10 +0100

    Update convertBamToFastq.sh

--- convertBamToFastq.sh
+++ convertBamToFastq.sh
@@ -21,7 +21,33 @@
 module purge
 module load bedtools/2.30.0
 
+Tourner en Boucle
 for i in *unmapped_reads_sorted.bam; do
 	base=$(basename $i _unmapped_reads_sorted.bam); echo ${base}_unmapped_reads_sorted.bam
 	bamToFastq -i ${base}_unmapped_reads_sorted.bam -fq ${base}_unmapped_reads_R1.fq -fq2 ${base}_unmapped_reads_R2.fq
 done 
+
+Tourner en array
+
+INPUT=(PATH/*unmapped_reads_sorted.bam)
+FILE=$(INPUT[$SLURM_ARRAY_TASK_ID-1])
+base=$(basename $(FILE) _unmapped_reads_sorted.bam)
+bamToFastq -i ${base}_unmapped_reads_sorted.bam -fq ${base}_unmapped_reads_R1.fq -fq2 ${base}_unmapped_reads_R2.fq
+
+
+	
+	
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+p { margin-bottom: 0.25cm; line-height: 115%; background: transparent }a:link { color: #000080; so-language: zxx; text-decoration: underline }
