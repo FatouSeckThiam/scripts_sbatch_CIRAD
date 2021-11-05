52ca877f519e846fc0ccae32291554a3d1af36af
Fatou Seck Thiam <86953064+FatouSeckThiam@users.noreply.github.com>
2021-11-05 17:11:23 +0100

    Update reads_unmapp.sh

--- reads_unmapp.sh
+++ reads_unmapp.sh
@@ -41,6 +41,6 @@ for i in *_unmapped_reads.bam; do
 	### 1) samtools sort ${base}.bam ${base}_sorted
 	### 2) samtools index -b ${base}_sorted.bam 
 	### 3) samtools view -b -f 12 ${base}_sorted.bam > ${base}_unmapped_reads.bam
-	samtools sort -n ${base}_unmapped_reads.bam ${base}_unmapped_reads_sorted (dernière partie à exécuter)
+	samtools sort -n ${base}_unmapped_reads.bam ${base}_unmapped_reads_sorted ###(dernière partie à exécuter)
 done
 
