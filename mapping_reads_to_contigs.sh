8bf090f538262f9f4986f8d2b9deb63dd7790e42
Fatou Seck Thiam <86953064+FatouSeckThiam@users.noreply.github.com>
2021-11-10 10:45:48 +0100

    Update mapping_reads_to_contigs.sh

--- mapping_reads_to_contigs.sh
+++ mapping_reads_to_contigs.sh
@@ -22,7 +22,6 @@ module load samtools/1.2
 ###module load bwa/0.7.17
 
 for infile in *_reads_mapping_sorted.bam; do
-	##base=$(echo ${infile} | sed "s/_L001_unmapped_reads_R1\.fq//"); echo ${base}.fasta ${base}_L001_unmapped_reads_R1.fq ${base}_L001_unmapped_reads_R2.fq
 	base=$(basename $infile _reads_mapping_sorted.bam); echo ${base}_reads_mapping_sorted.bam
 	###bwa index ${base}.fasta ${base}
 	###bwa mem -t 4 ${base}.fasta ../${base}_unmapped_reads_R1.fq  ../${base}_unmapped_reads_R2.fq | samtools view -bS - > ${base}_reads_mapping.bam
