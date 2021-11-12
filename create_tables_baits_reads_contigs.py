46e855fce786a83d40138f759b430123cc22c89a
Fatou Seck Thiam <86953064+FatouSeckThiam@users.noreply.github.com>
2021-11-12 10:57:35 +0100

    Update create_tables_baits_reads_contigs.py

--- create_tables_baits_reads_contigs.py
+++ create_tables_baits_reads_contigs.py
@@ -12,7 +12,7 @@ concat_baits=pd.concat((pd.read_csv(file, sep = " ", index_col = 0)
  # remplacer les valeurs nulle (NaN) par 0 et conversion en entier                    
 concat_baits=concat_baits.replace(np.nan, 0) 
 concat_baits=concat_baits.astype(int)
-blast_baits_contig = concat_baits.copy()
+#blast_baits_contig = concat_baits.copy()
  # exporter le tableau au format csv 
 concat_baits.to_csv("blast_baits_contig.csv", sep= ";")
 
@@ -28,39 +28,36 @@ concat_reads=concat_reads.astype(int)
 
 # exporter le tableau au format csv 
 concat_reads.to_csv("mapping_reads_contig.csv", sep=";")
-mapping_reads_contig = concat_reads.copy()
-###3) Définition des fonctions
 
-### Fonction pour creer la matrice binaire à partir d'un seuil de read( soit 5, 10 ou 20)
+# Lectures des 2 matrices
+blast_baits_contig=pd.read_csv("blast_baits_contig.csv", sep = ";", index_col = 0)
+mapping_reads_contig = pd.read_csv("mapping_reads_contig.csv", sep = ";", index_col = 0)
 
-def count_reads_with_seuil(df,seuil):
-    df[df >= seuil]=1 
-    df[df != 1]=0
-    df["Somme"]=df.sum(axis=1)
-    #output=df.to_csv(str(output), index= False)
-    return df
-
-###  Exemple pour appliquer la fonction avec un seuil de read à 5
-count_reads_with_seuil(mapping_reads_contig, 5)
-
-### Fonction pour compter le nombre de baits ayant X reads chez N individus (en fonction du seuil de reads de la fonction ci dessus)
-
-liste=np.arange(10, 230, 10)
+### 3) Définition des fonctions
+### Fonctions pour compter le nombre de baits ayant X reads chez N individus (en fonction du seuil de reads)
+   ### Fonction 1 
+liste=np.arange(10, 220, 10)
 df_liste=pd.DataFrame(liste, columns=["nombre_individus"])
 def count_baits(data,liste):
     value=[]
     for i in liste:
         a = len(data[(data["Somme"]>= i)])
         value.append(a)
-        Data=pd.DataFrame(value, columns=["nombre_baits_seuil_reads"])       
+        Data=pd.DataFrame(value, columns=["nombre_baits_seuil_reads"])        
         df_final=pd.concat([df_liste, Data], axis=1)
         df_final.to_csv("count_baits_ind_seuil.csv", sep = ";")
-    return df_final 
+    return df_final
+  
+  ### Fonction 2
+ def count_bait_all(mapping_reads_contig, seuil):
+    mapping_reads_Sm = mapping_reads_contig.copy()
+    mapping_reads_Sm[mapping_reads_Sm >= seuil] = 1
+    mapping_reads_Sm[mapping_reads_Sm != 1] = 0
+    mapping_reads_Sm["Somme"]=mapping_reads_Sm.sum(axis=1)
+    return count_baits(mapping_reads_Sm, liste)
 
 ### Fonction pour ne prendre en compte cette fois ci que les baits ayant 1 hit
-
 def bait_uniq(blast_baits_contig, mapping_reads_contig, minimum):
-    
     blast_baits_contig_1 = blast_baits_contig.copy()
     blast_baits_contig_1[blast_baits_contig_1 !=1] = 0
     mapping_reads_Sm = mapping_reads_contig.copy()
@@ -71,3 +68,5 @@ def bait_uniq(blast_baits_contig, mapping_reads_contig, minimum):
     blast_min = pd.concat([mapping_reads_Sort_Sm, blast_baits_contig_Sort_1]).min(level = 0)
     blast_min["Somme"] = blast_min.sum(axis = 1)
     return count_baits(blast_min, liste)
+  
+  
