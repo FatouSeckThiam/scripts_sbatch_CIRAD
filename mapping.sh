3b7f544fe187c6aef46f8da85d5be602be15051e
Fatou Seck Thiam <86953064+FatouSeckThiam@users.noreply.github.com>
2021-11-24 13:41:48 +0100

    Update mapping.sh

--- mapping.sh
+++ mapping.sh
@@ -12,7 +12,7 @@
 #SBATCH --partition=agap_long   ### Partition
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --array=0-353            ### Array index from 0 to 19 with 4 running jobs
+#SBATCH --array=0-353%20           
 #SBATCH --cpus-per-task=5
 #SBATCH --mem-per-cpu=4G
 
@@ -32,10 +32,11 @@ module load samtools/1.2
 
 ###bowtie2 -f -x Dalata -U Baits_Capture_RADlocus_Alata-Numularia-1.fasta -S mapping_Dalata_baits.sam
 ### l'option -f correspond au infput format fasta et l'option -q correspond aux input format fatq (comme les reads par rexemple)
-for infile in /home/thiamf/work_agap/DEFI/R1_R2_nuclear_data_trim/*1P.trimmed.fastq.gz; do
-base=$(echo ${infile} | sed "s/_1P.trimmed\.fastq\.gz//"); echo ${base}_1P.trimmed.fastq.gz ${base}_2P.trimmed.fastq.gz
-bowtie2 -p 4 -x Dalata_chloro -1 ${base}_1P.trimmed.fastq.gz -2 ${base}_2P.trimmed.fastq.gz | samtools view -bS - > ${base}.bam
-done
+
+FILES = ( /home/thiamf/work_agap/DEFI/R1_R2_nuclear_data_trim/*1P.trimmed.fastq.gz)
+read=${FILES[$SLURM_ARRAY_TASK_ID]}
+base=$(basename $read _1P.trimmed.fastq.gz)
+bowtie2 -x Dalata_chloro -1 ${base}_1P.trimmed.fastq.gz -2 ${base}_2P.trimmed.fastq.gz | samtools view -bS - > ${base}.bam
 
 
 
