3f5da41e0eb29cd898683591801f886396c6bd43
Fatou Seck Thiam <86953064+FatouSeckThiam@users.noreply.github.com>
2021-11-26 15:40:26 +0100

    Update count_reads_per_baits_ind.sh

--- count_reads_per_baits_ind.sh
+++ count_reads_per_baits_ind.sh
@@ -16,4 +16,4 @@
 #SBATCH --mem-per-cpu=4G
 
 
-for f in *.tsv; do indiv=$(basename $f .tsv); awk -v indiv=$indiv 'BEGIN{samcmd="samtools view -c /home/thiamf/scratch/Fatou/MAPP/nuclear/Trinity_assemblies/Contigs_ind/"indiv"_reads.bam"} {if($5<$6){m=$5;M=$6}else{m=$6; M=$5};if($1!=PREVBAITS){if(NR>1){print samcmd" "regions">"indiv" "PREVBAITS} regions=""; PREVBAITS=$1;} regions=regions $2":"m"-"M;  }END{print samcmd" "regions">"indiv" "PREVBAITS}' $f | awk -F">" '{print("nb=$("$1"); echo \"$nb "$2"\"")}' > getReads_count_$indiv.sh; done 
+for f in *.tsv; do indiv=$(basename $f .tsv); awk -v indiv=$indiv 'BEGIN{samcmd="samtools view -c /home/thiamf/scratch/Fatou/MAPP/nuclear/Trinity_assemblies/Contigs_ind/"indiv"_reads_mapping_sorted.bam"} {if($5<$6){m=$5;M=$6}else{m=$6; M=$5};if($1!=PREVBAITS){if(NR>1){print samcmd" "regions">"indiv" "PREVBAITS} regions=""; PREVBAITS=$1;} regions=regions $2":"m"-"M;  }END{print samcmd" "regions">"indiv" "PREVBAITS}' $f | awk -F">" '{print("nb=$("$1"); echo \"$nb "$2"\"")}' > getReads_count_$indiv.sh; done 
