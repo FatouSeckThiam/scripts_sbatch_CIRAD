d5359a0a99c6f16c99a67432849c40d721147a86
Fatou Seck Thiam <86953064+FatouSeckThiam@users.noreply.github.com>
2021-11-26 16:29:11 +0100

    Update count_contig_per_baits.sh

--- count_contig_per_baits.sh
+++ count_contig_per_baits.sh
@@ -1,5 +1,5 @@
 #!/bin/env bash
-
+# ce code permets de compter le nombre de contigs par baits, d'insérer les noms des indiviuds dans la première ligne de chaque fichier afin de pour créer le tableau en gardant les noms des individus
 for i in *L001.tsv; do base=$(basename $i _L001.tsv); awk '{c1[$1]++} END {for (e in c1) print e, c1[e]}' ${base}_L001.tsv | sed -e '1i\ \'${base} > ${base}_Conditg_GroupBy_Baits; done
 
 
