03cd46a0c781d3aaf5757a0a8ca998e4abab4cf9
Fatou Seck Thiam <86953064+FatouSeckThiam@users.noreply.github.com>
2021-11-30 00:16:05 +0100

    Update mapping_read_to_ref_chloro_alata.sh

--- mapping_read_to_ref_chloro_alata.sh
+++ mapping_read_to_ref_chloro_alata.sh
@@ -24,17 +24,27 @@ module purge
 module load bowtie2/2.4.2
 module load samtools/1.2
 
-###bowtie2 -f -x Dalata -U Baits_Capture_RADlocus_Alata-Numularia-1.fasta -S mapping_Dalata_baits.sam
-### l'option -f correspond au input format fasta et l'option -q correspond aux input format fastq (comme les reads par rexemple)
 
 # Dalata_chloro correspond à l'index de la séquence de référence Dalata chloroplaste
 # bowtie2-build Dalata_chloro_sequence_ref.fasta Dalata_chloro
 
+#1) Mapper les reads sur la référence chloroplase de Dalata
 FILES=(/home/thiamf/scratch/Fatou/Data_Fatou/02_Trimming/*1P.trimmed.fastq.gz)
 read=${FILES[$SLURM_ARRAY_TASK_ID]}
 base=$(basename $read _1P.trimmed.fastq.gz)
 bowtie2 -x Dalata_chloro -1 ${base}_1P.trimmed.fastq.gz -2 ${base}_2P.trimmed.fastq.gz | samtools view -bS - > ${base}.bam
-#Dalata_chloro correspond à l'index de la séquence de référence	Dalata chloroplaste
+
+
+#2) Les	fichiers bam doivent etre triés	et indexés pour extraire les reads nucleaires (non mappés sur la référence de Dalata chloro)
+#for i in *.bam; do
+#       base=$(basename $i .bam); echo ${base}.bam
+#       samtools sort ${base}.bam ${base}_sorted
+#       samtools index -b ${base}_sorted.bam
+
+
+#done
+
+
 
 
 
