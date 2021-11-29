8106c58d7d5375a902ddea751542d36ffbbddc91
Fatou Seck Thiam <86953064+FatouSeckThiam@users.noreply.github.com>
2021-11-29 23:54:35 +0100

    Update and rename extract_reads_nucl_unmapp.sh to extract_reads_nuclear.sh

---
+++ extract_reads_nuclear.sh
@@ -0,0 +1,34 @@
+#!/bin/env bash
+
+#
+
+#SBATCH -J map               ### Job name
+
+#SBATCH -o map."%j".out          ### Standard output
+
+#SBATCH -e map."%j".err          ### Standard error
+
+#
+#SBATCH --partition=agap_long   ### Partition
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --array=0-113%10            ### Array index from 0 to 19 with 4 running jobs
+#SBATCH --cpus-per-task=5
+#SBATCH --mem-per-cpu=4G
+
+
+
+module purge
+
+
+module load bowtie2/2.4.2
+module load samtools/1.2
+
+for i in *_nuclear_reads.bam; do
+	base=$(basename $i _nuclear_reads.bam); echo ${base}_nuclear_reads.bam
+	###samtools sort ${base}.bam ${base}_sorted 
+	###samtools index -b ${base}_sorted.bam 
+	###samtools view -b -f 12 ${base}_sorted.bam > ${base}_nuclear_reads.bam
+	samtools sort -n ${base}_nuclear_reads.bam ${base}_nuclear_reads_sorted
+done
+
