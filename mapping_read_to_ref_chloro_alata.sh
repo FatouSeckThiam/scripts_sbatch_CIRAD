ec7aa699f28366a906e9b5ef87a07dba9aabd55d
Fatou Seck Thiam <86953064+FatouSeckThiam@users.noreply.github.com>
2021-11-29 22:44:52 +0100

    Update mapping_read_to_ref_chloro_alata.sh

--- mapping_read_to_ref_chloro_alata.sh
+++ mapping_read_to_ref_chloro_alata.sh
@@ -20,20 +20,14 @@
 
 module purge
 
-###module load bwa-mem2/2.0
+
 module load bowtie2/2.4.2
 module load samtools/1.2
 
-###R1=(/home/thiamf/work_agap/DEFI/R1_R2_nuclear_data_trim/*1P.trimmed.fastq.gz)
-###R2=(/home/thiamf/work_agap/DEFI/R1_R2_nuclear_data_trim/*2P.trimmed.fastq.gz)
-###bowtie2 -x Dalata -1 ${R1[${SLURM_ARRAY_TASK_ID}]} -2 ${R2[${SLURM_ARRAY_TASK_ID}]} -S out.sam 
- 
-###bowtie2  -x Dalata -U Baits_Capture_RADlocus_Alata-Numularia-1.fasta -S align.sam
-
 ###bowtie2 -f -x Dalata -U Baits_Capture_RADlocus_Alata-Numularia-1.fasta -S mapping_Dalata_baits.sam
-### l'option -f correspond au infput format fasta et l'option -q correspond aux input format fatq (comme les reads par rexemple)
+### l'option -f correspond au input format fasta et l'option -q correspond aux input format fastq (comme les reads par rexemple)
 
-FILES = ( /home/thiamf/work_agap/DEFI/R1_R2_nuclear_data_trim/*1P.trimmed.fastq.gz)
+FILES=(/home/thiamf/scratch/Fatou/Data_Fatou/02_Trimming/*1P.trimmed.fastq.gz)
 read=${FILES[$SLURM_ARRAY_TASK_ID]}
 base=$(basename $read _1P.trimmed.fastq.gz)
 bowtie2 -x Dalata_chloro -1 ${base}_1P.trimmed.fastq.gz -2 ${base}_2P.trimmed.fastq.gz | samtools view -bS - > ${base}.bam
