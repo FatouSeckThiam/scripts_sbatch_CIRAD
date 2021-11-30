63d9c503ba83e4a01fd415410709329a546a33ad
Fatou Seck Thiam <86953064+FatouSeckThiam@users.noreply.github.com>
2021-11-30 14:36:19 +0100

    Update trinity.sh

--- trinity.sh
+++ trinity.sh
@@ -23,11 +23,13 @@ module load python/3.4.10
 #Tourner en boucle
 #for i in /home/thiamf/scratch/Fatou/Data_Fatou/06_BamToFastq_Reads/*nuclear_reads_R1.fq; do
 #	base=$(basename $i _nuclear_reads_R1.fq); echo ${base}_nuclear_reads_R1.fq ${base}_nuclear_reads_R2.fq
-#	Trinity --seqType fq --no_bowtie --left ${base}_nuclear_reads_R1.fq --right ${base}_nuclear_reads_R2.fq  --max_memory 5G --CPU 5 --output ${base}.trinity
+#	Trinity --seqType fq --left ${base}_nuclear_reads_R1.fq --right ${base}_nuclear_reads_R2.fq  --max_memory 5G --CPU 5 --output ${base}.trinity
 #done
 
 #Tourner en Array
 INPUT=(/home/thiamf/scratch/Fatou/Data_Fatou/06_BamToFastq_Reads/*nuclear_reads_R1.fq)
 FILE=$(INPUT[$SLURM_ARRAY_TASK_ID-1])
 base=$(basename $(FILE) _nuclear_reads_sorted.bam)
-Trinity --seqType fq --no_bowtie --left ${base}_nuclear_reads_R1.fq --right ${base}_nuclear_reads_R2.fq  --max_memory 5G --CPU 5 --output ${base}.trinity
+Trinity --seqType fq --left ${base}_nuclear_reads_R1.fq --right ${base}_nuclear_reads_R2.fq  --max_memory 5G --CPU 5 --output ${base}.trinity
+
+
