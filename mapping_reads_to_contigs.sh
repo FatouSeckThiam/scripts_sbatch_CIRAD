a4f131880fd3e31f2afe0de39b6c62f560ec98a4
Fatou Seck Thiam <86953064+FatouSeckThiam@users.noreply.github.com>
2021-11-30 15:07:57 +0100

    Update mapping_reads_to_contigs.sh

--- mapping_reads_to_contigs.sh
+++ mapping_reads_to_contigs.sh
@@ -20,14 +20,14 @@
 
 module purge
 module load samtools/1.2
-###module load bwa/0.7.17
+###module load bwa/0.7.17    #enlever commentaire pour faire le mapping
 bamfile=(*_reads_mapping_sorted.bam)
 input=${bamfile[$SLUR_ARRAY_TASK_ID]}
 base=$(basename $input _reads_mapping_sorted.bam)
-###bwa index ${base}.fasta ${base}
-###bwa mem -t 4 ${base}.fasta ../${base}_unmapped_reads_R1.fq  ../${base}_unmapped_reads_R2.fq | samtools view -bS - > ${base}_reads_mapping.bam
-###samtools sort ${base}_reads_mapping.bam ${base}_reads_mapping_sorted
-samtools index ${base}_reads_mapping_sorted.bam 	
+#1) bwa index ${base}.fasta ${base}
+#2) bwa mem -t 4 ${base}.fasta ../${base}_unmapped_reads_R1.fq  ../${base}_unmapped_reads_R2.fq | samtools view -bS - > ${base}_reads_mapping.bam
+#3) samtools sort ${base}_reads_mapping.bam ${base}_reads_mapping_sorted
+#4) samtools index ${base}_reads_mapping_sorted.bam 	
 ###les fichiers	R1.fq et R2.fq sont le répertoire 06_BamToFastq_Reads
 
 
