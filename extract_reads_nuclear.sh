14fcca992273f853e8e0b9ce5474aafc152198f5
Fatou Seck Thiam <86953064+FatouSeckThiam@users.noreply.github.com>
2021-12-02 14:09:32 +0100

    Update extract_reads_nuclear.sh

--- extract_reads_nuclear.sh
+++ extract_reads_nuclear.sh
@@ -12,7 +12,7 @@
 #SBATCH --partition=agap_long   ### Partition
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --array=0-113%10            ### Array index from 0 to 19 with 4 running jobs
+#SBATCH --array=0-113%10            ### Penser à adapter le nombre d'array en fonction du nombre d'échantillons
 #SBATCH --cpus-per-task=5
 #SBATCH --mem-per-cpu=4G
 
@@ -25,8 +25,8 @@ module load bowtie2/2.4.2
 module load samtools/1.2
 
 for i in *_nuclear_reads.bam; do
-	base=$(basename $i _nuclear_reads.bam); echo ${base}_nuclear_reads.bam
-	###samtools view -b -f 12 ${base}_sorted.bam > ${base}_nuclear_reads.bam
-	samtools sort -n ${base}_nuclear_reads.bam ${base}_nuclear_reads_sorted
+        base=$(basename $i _nuclear_reads.bam); echo ${base}_nuclear_reads.bam
+        #1) samtools view -b -f 12 ${base}_sorted.bam > ${base}_nuclear_reads.bam    #extraction des reads nucleaires au format bam avec l'option f 12 de samtools view
+        #2) samtools sort -n ${base}_nuclear_reads.bam ${base}_nuclear_reads_sorted  #Tri des fichier bam nucleaires par nom (pas par postion) avec l'option -n de samtools sort
 done
 
