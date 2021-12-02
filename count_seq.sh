246b49af337090259c5a33d0c79984fa8025c2e4
Fatou Seck Thiam <86953064+FatouSeckThiam@users.noreply.github.com>
2021-12-02 11:50:08 +0100

    Update count_seq.sh

--- count_seq.sh
+++ count_seq.sh
@@ -21,7 +21,7 @@
 
 
 
-liste= ls -la R1_R2_nuclear_data_trim/*2P.trimmed.fastq.gz |awk 'split($9, a,"/"){print a[2]}'|awk -F"_" '{print $1}'
+liste= ls -la 00_Raw_Reads/*R1*.fastq.gz |awk 'split($9, a,"/"){print a[2]}'|awk -F"_" '{print $1}'
 for i in ${liste}; do zcat $i | echo -ne $i "\t" $((`wc -l`/4)); done > liste_count
 
 ####for i in $(less liste_count); do echo $(($i*2)); done > count_reads_trim
