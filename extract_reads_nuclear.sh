ff80a1c2f1775b5193283945ffebd14894f8bc5b
Fatou Seck Thiam <86953064+FatouSeckThiam@users.noreply.github.com>
2021-12-03 15:38:30 +0100

    Update extract_reads_nuclear.sh

--- extract_reads_nuclear.sh
+++ extract_reads_nuclear.sh
@@ -12,7 +12,7 @@
 #SBATCH --partition=agap_long   ### Partition
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --array=0-113%10            ### Penser à adapter le nombre d'array en fonction du nombre d'échantillons
+#SBATCH --array=0-222%20         ###  Penser à adapter le nombre d'array en fonction du nombre d'échantillons
 #SBATCH --cpus-per-task=5
 #SBATCH --mem-per-cpu=4G
 
@@ -21,12 +21,15 @@
 module purge
 
 
-module load bowtie2/2.4.2
 module load samtools/1.2
 
-for i in *_nuclear_reads.bam; do
-        base=$(basename $i _nuclear_reads.bam); echo ${base}_nuclear_reads.bam
+#Penser à adapter les chemins des fichiers d'entrée en cas de besoin.
+
+for i in PATH/04_Mapping_Reads_To_Ref_Chloro/*_sorted.bam; do
+        base=$(basename $i _sorted.bam); echo ${base}_sorted.bam
         #1) samtools view -b -f 12 ${base}_sorted.bam > ${base}_nuclear_reads.bam    #extraction des reads nucleaires au format bam avec l'option f 12 de samtools view
-        #2) samtools sort -n ${base}_nuclear_reads.bam ${base}_nuclear_reads_sorted  #Tri des fichier bam nucleaires par nom (pas par postion) avec l'option -n de samtools sort
+        #2) samtools sort -n ${base}_nuclear_reads.bam ${base}_nuclear_reads_sorted  #Tri des fichiers bam nucleaires par nom (non pas par postion) avec l'option -n de samtools sort
 done
+
+
 
