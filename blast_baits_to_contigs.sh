b6bc696baba8298267aef58cef2cf527341b3e92
Fatou Seck Thiam <86953064+FatouSeckThiam@users.noreply.github.com>
2021-12-03 17:19:47 +0100

    Update blast_baits_to_contigs.sh

--- blast_baits_to_contigs.sh
+++ blast_baits_to_contigs.sh
@@ -13,6 +13,7 @@
 #SBATCH --partition=agap_long   ### Partition
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
+#SBATCH --array=0-211%50
 #SBATCH --cpus-per-task=5
 #SBATCH --mem-per-cpu=4G
 
@@ -22,7 +23,7 @@ module load ncbi-blast/2.6.0+-bin
 
 for infile in *fasta; do
 	base=$(echo ${infile} | sed "s/\.fasta//"); echo ${base}.fasta
-	#1) makeblastdb -in ${base}.fasta -title ${base} -out ${base} -dbtype nucl # pour creer la base de données à partir des contig (.fasta)
+	#1) makeblastdb -in ${base}.fasta -title ${base} -out ${base} -dbtype nucl #pour creer la base de données à partir des contig (.fasta)
 	#2) blastn -query Baits_Capture_RADlocus_Alata-Numularia-1.fasta -db ${base} -outfmt "6 qseqid sseqid qlen slen sstart send qstart qend lenght evalue" > ${base}.tsv #partie blast
 
 done
